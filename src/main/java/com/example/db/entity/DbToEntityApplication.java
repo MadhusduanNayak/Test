003c8685472ef64926dd0c4cacb45d24bf30bdf6
Madhusudan Nayak <t_madhusudan.n@tatadigital.com>
2023-12-26 14:58:11 +0530

    test

---
+++ src/main/java/com/example/db/entity/DbToEntityApplication.java
@@ -0,0 +1,13 @@
+package com.example.db.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbToEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbToEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/db/entity/DbToEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbToEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
